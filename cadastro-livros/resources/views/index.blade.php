8631aa6043c1a6e116660733419f5af6e639ecda
FelipeTerra <38055797+FelipeTerra@users.noreply.github.com>
2021-08-02 14:31:18 -0400

    Criação do Layout
    Criado Layout Principal, tela inicial e nav para a Inclusão/Edição/Exclusão de um livro.

---
+++ cadastro-livros/resources/views/index.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.principal')
+
+@section('main')
+
+<div class="container">
+    <div class="py-5 text-center">
+        <h2>Livros Cadastrados</h2>
+    </div>
+    <div class="row">
+        <!-- Título -->
+        <div class="col-md-2">
+            <h5>Título</h5>
+            {{-- TODO --}}
+        </div>
+
+        <!-- Autor Principal -->
+        <div class="col-md-2">
+            <h5>Autor Principal</h5>
+            {{-- TODO --}}
+        </div>
+
+        <!-- ISBN -->
+        <div class="col-md-2">
+            <h5>ISBN</h5>
+            {{-- TODO --}}
+        </div>
+        
+        <!-- Preço -->
+        <div class="col-md-2">
+            <h5>Preço</h5>
+            {{-- TODO --}}
+        </div>
+
+        <!-- Editora -->
+        <div class="col-md-2">
+            <h5>Editora</h5>
+            {{-- TODO --}}
+        </div>
+
+        <!-- Ano de Lançamento -->
+        <div class="col-md-2">
+            <h5>Ano Lançamento</h5>
+            {{-- TODO --}}
+        </div>
+
+
+    </div>
+</div>
+    
+@endsection
